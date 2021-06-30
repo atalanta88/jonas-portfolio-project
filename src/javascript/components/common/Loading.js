4376d3dd35958f4e68c6e404647cc9dcf5c780ce
atalanta88 <jonasold@hotmail.com>
2021-06-30 23:50:21 +0200

    loader centered

--- src/javascript/components/common/Loading.js
+++ src/javascript/components/common/Loading.js
@@ -2,9 +2,17 @@ import React from "react";
 import Loader from "react-loader-spinner";
 
 export default function LoaderComponent() {
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
   return (
     <>
-      <Loader type="Puff" color="#00BFFF" height={100} width={100} />{" "}
+      <div style={style}>
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} />{" "}
+      </div>
     </>
   );
 }
