91e7bc51dabaf67cd9580c91d6f3ded08b4356a8
atalanta88 <jonasold@hotmail.com>
2021-08-19 14:25:57 +0200

    contact form able to send mails

--- src/javascript/components/sections/contact/Contact.js
+++ src/javascript/components/sections/contact/Contact.js
@@ -1,4 +1,35 @@
+import React from "react";
+import emailjs from "emailjs-com";
+import { Form, Input, TextArea } from "semantic-ui-react";
+import Swal from "sweetalert2";
+
+const SERVICE_ID = "service_qabfs9j";
+const TEMPLATE_ID = "template_cqvwfe8";
+const USER_ID = "user_5TQfYXjk2s5wf6PRM0Vwg";
+
 export default function ContactSection() {
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, event.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong",
+          text: error.text,
+        });
+      }
+    );
+    event.target.reset();
+  };
+
   return (
     <>
       {" "}
@@ -15,7 +46,11 @@ export default function ContactSection() {
             loading="lazy"
           ></iframe>
         </div>
-        <div className="container px-5 py-24 mx-auto flex">
+
+        <Form
+          className="container px-5 py-24 mx-auto flex"
+          onSubmit={handleOnSubmit}
+        >
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
               Send en melding
@@ -23,38 +58,28 @@ export default function ContactSection() {
             <p className="leading-relaxed mb-5 text-gray-600">
               Jeg vil svare så fort som mulig
             </p>
-            <div className="relative mb-4">
-              <label for="name" className="leading-7 text-sm text-gray-600">
-                Navn
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              ></input>
-            </div>
-            <div className="relative mb-4">
-              <label for="email" className="leading-7 text-sm text-gray-600">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              ></input>
-            </div>
-            <div className="relative mb-4">
-              <label for="message" className="leading-7 text-sm text-gray-600">
-                Melding
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              ></textarea>
-            </div>
+            <Form.Field
+              control={Input}
+              name="reply_to"
+              placeholder="Email…"
+              required
+              icon="mail"
+              iconPosition="left"
+            />
+            <Form.Field
+              control={Input}
+              name="from_name"
+              placeholder="Navn..."
+              required
+              icon="user circle"
+              iconPosition="left"
+            />
+            <Form.Field
+              control={TextArea}
+              name="message"
+              placeholder="Melding..."
+              required
+            />
             <button
               type="submit"
               className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
@@ -62,8 +87,69 @@ export default function ContactSection() {
               Send
             </button>
           </div>
-        </div>
+        </Form>
       </section>{" "}
     </>
   );
 }
+
+/*          <div className="absolute inset-0 bg-gray-300">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19473.67399598583!2d10.214749373502187!3d59.13377487630202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4646c090553a802b%3A0xc35b51a25c3ce1a2!2sSandefjord!5e0!3m2!1sno!2sno!4v1624869469571!5m2!1sno!2sno"
+              width="100%"
+              height="100%"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+          <div className="container px-5 py-24 mx-auto flex">
+            <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+                Send en melding
+              </h2>
+              <p className="leading-relaxed mb-5 text-gray-600">
+                Jeg vil svare så fort som mulig
+              </p>
+              <div className="relative mb-4">
+                <label for="name" className="leading-7 text-sm text-gray-600">
+                  Navn
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                ></input>
+              </div>
+              <div className="relative mb-4">
+                <label for="email" className="leading-7 text-sm text-gray-600">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                ></input>
+              </div>
+              <div className="relative mb-4">
+                <label
+                  for="message"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Melding
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              >
+                Send
+              </button>
+            </div>
+          </div>*/
